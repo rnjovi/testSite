43331e01da7bf253ee7787344142b8f74c5d181f
vronhass <joona.vihervaara@gmail.com>
2023-01-14 13:46:33 +0200

    modified app

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-document.body.innerHTML = "This is content";
+const message = 'Hit' 
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+// Log to console
+console.log(message)
